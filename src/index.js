101cb2c1a25c9aebc60cead5d21ba7ea03f3193b
Victor Torres Moreno <victor.torres_taking@totalexpress.com.br>
2022-06-02 15:12:29 -0300

    added server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const usersRoutes = require('./routes/users');
+const authenticationRoutes = require('./routes/authentication');
+
+const app = express();
+
+app.use(express.json());
+app.use('/usuario', usersRoutes);
+app.use('/login', authenticationRoutes);
+
+app.listen(8000, () => {
+    console.log('Server running on port http://localhost:8000');
+});
