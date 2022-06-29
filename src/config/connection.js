6d64a87515ade6e6821a7f1abd609003db1dd826
Victor Torres Moreno <victor.torres_taking@totalexpress.com.br>
2022-06-28 21:36:20 -0300

    configurando conexao para ssl off

--- src/config/connection.js
+++ src/config/connection.js
@@ -5,7 +5,10 @@ const pool = new Pool({
     host: 'ec2-18-204-142-254.compute-1.amazonaws.com',
     database: 'd2v7m3caj5hvnd',
     password: '517ef3a2428bf4075fb3b3b5c185b9a6e578d5e6c0ac0a342eeaaf37b0a603b8',
-    port: 5432
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const query = (text, param) => {
