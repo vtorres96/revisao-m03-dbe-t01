d2c4a55368010ff420f005f84821aa13d3e4d937
Victor Torres Moreno <victor.torres_taking@totalexpress.com.br>
2022-06-28 21:14:50 -0300

    alterando propriedades de conexao com banco de dados para utilizar banco de deploy

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'revisao_unidade_tres',
-    password: '03v01t96m',
+    user: 'wntenayefznoce',
+    host: 'ec2-18-204-142-254.compute-1.amazonaws.com',
+    database: 'd2v7m3caj5hvnd',
+    password: '517ef3a2428bf4075fb3b3b5c185b9a6e578d5e6c0ac0a342eeaaf37b0a603b8',
     port: 5432
 });
 
